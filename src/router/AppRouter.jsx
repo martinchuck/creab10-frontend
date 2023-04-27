118e3fe6204e4445310e4bae161e848f7e20bbc3
martin <martinguettejp@unimagdalena.edu.co>
2023-04-27 17:38:14 -0500

    add main page

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from '../components/LoginPage';
 import RegisterPage from '../components/RegisterPage';
+import { MainPage } from '../components/MainPage';
 
 export const AppRouter = () => {
 return (
     <>
     <Routes>
         <Route index element={
-        <LoginPage />
+        <MainPage />
         } />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/login" element={<MainPage />} />
         <Route path="/register" element={<RegisterPage />} />
 
     </Routes>
