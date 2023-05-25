761bcff216951d73cf7836313b7eaa6dff0f45d7
martin <martinguettejp@unimagdalena.edu.co>
2023-05-25 11:53:15 -0500

    trabajando card cursos

--- src/components/CoursesCard.jsx
+++ src/components/CoursesCard.jsx
@@ -9,21 +9,28 @@ function CoursesCard({ course }) {
   const { deleteCourse } = useContext(CourseContext);
 
   return (
-    <Grid item md={2} >
+    <Grid item
+    xs={12}
+    sm={6}
+    md={4}
+    lg={3}
+    xl={2}
+     >
          <Paper elevation={8} sx={{ 
          maxWidth: 250,
+         overflow: 'hidden',
          minHeight: 250,
          maxHeight: 250,
          borderRadius: 2,
           }}>
     <CardActionArea>
-    <CardHeader title={
+    <CardHeader  title={
               <Typography gutterBottom variant="h5" component="h2">
         {course.name}
         </Typography>
     }
     subheader={
-      <Typography variant="body2" color="#757575" component="p" >
+      <Typography variant="body2" color="#757575" component="p"  >
         {course.description}
         </Typography>
     }
