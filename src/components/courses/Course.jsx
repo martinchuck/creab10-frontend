cd12e3258c6d3544f55f0da92e0d2e5723624dbc
martin <martinguettejp@unimagdalena.edu.co>
2023-06-01 11:48:02 -0500

    editor js funcionando con box tipo paper

--- src/components/courses/Course.jsx
+++ src/components/courses/Course.jsx
@@ -1,10 +1,9 @@
 import React, { useEffect } from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Container } from "@mui/material";
 import {  useNavigate } from "react-router-dom";
 import { getAuthToken, } from "../../shared/login";
 import EditorJS from '@editorjs/editorjs';
 import ConfigurationEditor from "./ConfigurationEditor";
-
 import NavBar from "../navbar/NavBar";
 
 function Course({ navArrayLinks }) {
@@ -20,15 +19,27 @@ function Course({ navArrayLinks }) {
 
   return (
     <>
-      <Box sx={{ flexGrow: 1 }}>
+      <Box sx={{ flexGrow: 1, 
+       }}>
         <NavBar navArrayLinks={navArrayLinks} />
         <Typography variant="h1" color="initial" sx={{
           fontWeight: "700",
           fontSize: "2rem",
+          m: 1, mx: 2
         }}
         >
           Curso
         </Typography>
+        <Container sx={{
+          bgcolor: "background.paper",
+          display: "flex",
+          justifyContent: "",
+          borderRadius: 6,
+          p: 2,
+        }}>
+          
+        <div id="editorjs"></div>
+        </Container>
       </Box>
     </>
   );
